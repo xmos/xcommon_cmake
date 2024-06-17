0c586c158bea5593ca1ed43481d79211c5a192ac
Ross Owen <ross@xmos.com>
2024-06-17 15:05:33 +0100

    Update to test lib_static_lib_flags for new static lib handling strategy

--- tests/static_lib_srcs_flags/lib_static0/lib_static0/lib_build_info.cmake
+++ tests/static_lib_srcs_flags/lib_static0/lib_static0/lib_build_info.cmake
@@ -1,14 +1,19 @@
 set(LIB_NAME lib_static0)
 set(LIB_VERSION 1.0.0)
-set(LIB_ARCH xs3a xs2a)
 set(LIB_INCLUDES api)
 set(LIB_DEPENDENT_MODULES "")
 
 set(LIB_ADD_SRC_DIRS src)
 set(LIB_ADD_INC_DIRS inc)
 
+set(LIB_ARCHIVES archive_xs3 archive_xs2)
+
+set(LIB_ARCHIVE_ARCH_archive_xs3 xs3a
+set(LIB_ARCHIVE_ARCH_archive_xs2 xs2a
+
 # Applies to building the .a
-set(LIB_COMPILER_FLAGS_xs3a -DLIB_STATIC0_OPTION_xs3a=1)
+set(LIB_ARCHIVE_FLAGS_archive_xs2 -DLIB_STATIC0_OPTION_archive_xs3=1)
+set(LIB_ARCHIVE_FLAGS_archive_xs3 -DLIB_STATIC0_OPTION_archive_xs2=1)
 
 # Applies to the additional source files
 set(LIB_COMPILER_FLAGS -DLIB_STATIC0_OPTION=1)
--- tests/static_lib_srcs_flags/lib_static0/lib_static0/src/static0_aux.xc
+++ tests/static_lib_srcs_flags/lib_static0/lib_static0/src/static0_aux.xc
@@ -1,7 +1,15 @@
 #include "static0_aux.h"
 
 #ifndef LIB_STATIC0_OPTION
-#error "This should be defined"
+#error "Expecting LIB_STATIC0_OPTION to be defined here"
+#endif
+
+#ifdef LIB_STATIC0_OPTION_archive_xs3
+#error "Not expecting LIB_STATIC0_OPTION_archive_xs3 to be defined here"
+#endif
+
+#ifdef LIB_STATIC0_OPTION_archive_xs2
+#error "Not expecting LIB_STATIC0_OPTION_archive_xs2 to be defined here"
 #endif
 
 void static0_aux_xc() {}
