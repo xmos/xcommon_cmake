6ba8e29ad003ae25392ab4f30572d5af8921524b
Ross Owen <ross@xmos.com>
2024-06-17 13:50:13 +0100

    Test static_lib_arch: Proposed updates for static lib handling

--- tests/static_lib_arch/lib_static0/lib_static0/lib_build_info.cmake
+++ tests/static_lib_arch/lib_static0/lib_static0/lib_build_info.cmake
@@ -1,6 +1,11 @@
 set(LIB_NAME lib_static0)
 set(LIB_VERSION 1.0.0)
-set(LIB_ARCH xs2a xs3a)
+
+set(LIB_ARCHIVES archive_xs2 archive_xs3)
+
+set(LIB_ARCHIVE_ARCH_archive_xs2 xs2a)
+set(LIB_ARCHIVE_ARCH_archive_xs3 xs3a)
+
 set(LIB_INCLUDES api)
 set(LIB_DEPENDENT_MODULES "")
 
