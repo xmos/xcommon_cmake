2404eb81189c8b3b53013099da60361462b0fa8b
Ross Owen <ross@xmos.com>
2024-06-18 12:28:33 +0100

    Added test: static_lib_unnamed

---
+++ tests/static_lib_unnamed/README.txt
@@ -0,0 +1,2 @@
+Static library is built for multiple architectures but isn't named. i.e. only
+achives for each architecture are built.
---
+++ tests/static_lib_unnamed/lib_static0/lib_static0/lib_build_info.cmake
@@ -0,0 +1,9 @@
+set(LIB_NAME lib_static0)
+set(LIB_VERSION 1.0.0)
+
+set(LIB_ARCHIVE_ARCHS xs2a xs3a)
+
+set(LIB_INCLUDES api)
+set(LIB_DEPENDENT_MODULES "")
+
+XMOS_REGISTER_STATIC_LIB()
